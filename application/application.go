d4e31dfd437b1dd30c223d2b9a9636969e464984
Nuno Morais <nm.morais@campus.fct.unl.pt>
2021-06-06 21:44:04 +0100

    increase prepare time

--- application/application.go
+++ application/application.go
@@ -130,7 +130,7 @@ func NewApplicationProtocol(babel protocolManager.ProtocolManager, isPlumtreeRoo
 	logger := logs.NewLogger(Name)
 
 	csvHeaders := []string{"ip", "timestamp", "time", "hopNr", "mid"}
-	prepareTime := time.Duration(float32(scenarioDuration) * 0.40)
+	prepareTime := time.Duration(float32(scenarioDuration) * 0.50)
 	runTime := time.Duration(float32(scenarioDuration-prepareTime) * 0.2)
 	if isPlumtreeRoot {
 		prepareTime -= time.Duration(float32(scenarioDuration) * 0.1)
